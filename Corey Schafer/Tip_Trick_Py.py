1abcf359a9afc409f60f4168f81306c4d91cb84d
unknown <ng.tanthanh27@gmail.com>
2020-09-17 22:19:42 +0700

    Trick+Tip.py

---
+++ Corey Schafer/Tip_Trick_Py.py
@@ -0,0 +1,15 @@
+'1 Dùng if theo cách khác'
+# condition = True
+# if condition:
+#     x = 1
+# else:
+#     x = 0
+# print (x)
+# 'or'
+# x = 1 if condition else 0
+# print (x)
+'Đọc số khó đọc'
+num1 = 10_000_000_000
+num2 = 10_000_000
+total = num1 + num2
+print (total)
